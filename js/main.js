66cae07379923d2dbbd5fde6a4f16b536a8780cd
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-18 16:31:48 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let utenti = ["luca", "mario", "luigi", "stocazzo"]
+let utenti = ["luca", "mario", "luigi"]
 let logUser = prompt( "inserisciCredenziali" )
 for (let x = 0; x < utenti.length; x++) {
     let utente = (utenti[x])
