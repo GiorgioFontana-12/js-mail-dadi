a301b8aeb2ad1fae14fa281cd20affe33a60e501
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-18 16:57:19 +0200

    need help

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-let utenti = ["luca", "mario", "luigi"]
-let logUser = prompt( "inserisciCredenziali" )
-for (let x = 0; x < utenti.length; x++) {
-    let utente = (utenti[x])
-    if (logUser == utente) {
-        alert ("sei loggato")
-        
+let utenti = ["giorgiofontanaj@gmail.com", "giorgiofontanaj@icloud.com", "pinomauro@gmail.com"]
+let logUser = (document.getElementById("inputEmail1").value)
+if (logUser != "") {
+    for (let x = 0; x < utenti.length; x++) {
+        let utente = (utenti[x])
+        console.log (utente)
+        if (logUser == utente) {
+            alert ("sei loggato")
+        }else {
+            alert ("credenziali inesistenti")
+        }
     }
+}else {
+
 }
