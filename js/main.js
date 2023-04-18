f22ab31b23a166633d7b18c1eba1124880f3258e
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-18 17:28:34 +0200

    need help4

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 let utenti = ["giorgiofontanaj@gmail.com", "giorgiofontanaj@icloud.com", "pinomauro@gmail.com"]
 let logUser = (document.getElementById("inputEmail1").value)
 const form = document.getElementById("form");
+let verificaUser = false;
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     for (let x = 0; x < utenti.length; x++) {
         let utente = (utenti[x])
         console.log (utente)
         if (logUser == utente) {
-            alert ("sei loggato")
-        }else {
-            alert ("credenziali inesistenti")
+            verificaUser = true;
         }
     }
 });
+
+if (verificaUser == true ) {
+    alert ("user loggato ")
+}else {
+    alert ("user inesistente")
+}
