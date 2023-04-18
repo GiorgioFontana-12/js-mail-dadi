a059aa1f475fca046fed8ff97085f40c2f362806
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-18 17:36:17 +0200

    end project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let utenti = ["giorgiofontanaj@gmail.com", "giorgiofontanaj@icloud.com", "pinomauro@gmail.com"]
-let logUser = (document.getElementById("inputEmail1").value)
 const form = document.getElementById("form");
 let verificaUser = false;
 form.addEventListener("submit", function(event) {
+    let logUser = (document.getElementById("inputEmail1").value)
     event.preventDefault();
     for (let x = 0; x < utenti.length; x++) {
         let utente = (utenti[x])
@@ -11,10 +11,9 @@ form.addEventListener("submit", function(event) {
             verificaUser = true;
         }
     }
-});
-
 if (verificaUser == true ) {
     alert ("user loggato ")
 }else {
     alert ("user inesistente")
 }
+});
